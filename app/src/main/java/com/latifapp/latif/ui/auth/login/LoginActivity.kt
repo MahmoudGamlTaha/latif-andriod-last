2c326ac570be9568a00055b61db34c24951f1667
Sara Ahmed <sara.mk.403@gmail.com>
2021-05-17 21:12:44 +0200

    pulling

--- app/src/main/java/com/latifapp/latif/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/latifapp/latif/ui/auth/login/LoginActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.lifecycle.Observer
+import com.latifapp.latif.R
 import com.latifapp.latif.data.local.PreferenceConstants.Companion.Lang_PREFS
 import com.latifapp.latif.databinding.ActivityLoginBinding
 import com.latifapp.latif.ui.auth.signup.SignUpActivity
@@ -12,6 +14,7 @@ import com.latifapp.latif.ui.base.BaseActivity
 import com.latifapp.latif.ui.main.home.MainActivity
 import com.latifapp.latif.utiles.Utiles
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_login.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
@@ -32,11 +35,25 @@ class LoginActivity :BaseActivity<LoginViewModel,ActivityLoginBinding>() {
 
         }
 
+        viewModel.validateLiveData.observe(this, Observer {
+            if (it !=null){
+                when(it){
+                    LoginViewModel.SignUpFiled.email_->
+                        binding.usernameEx.setError(getString(R.string.usernameError))
+                    LoginViewModel.SignUpFiled.password_->
+                        binding.passwordEx.setError(getString(R.string.passwordError))
+                }
+            }
+        })
         binding.signUpBtn.setOnClickListener {
+            binding.usernameEx.setError(null)
+            binding.passwordEx.setError(null)
             startActivity(Intent(this,SignUpActivity::class.java))
         }
         binding.loginBtn.setOnClickListener {
-            startActivity(Intent(this,MainActivity::class.java))
+            //startActivity(Intent(this,MainActivity::class.java))
+            viewModel.login(binding.usernameEx.text.toString(),
+                binding.passwordEx.text.toString())
         }
      }
 
@@ -47,10 +64,10 @@ class LoginActivity :BaseActivity<LoginViewModel,ActivityLoginBinding>() {
 
 
     override fun showLoader() {
-       // binding.loader.bar.visibility = View.VISIBLE
+         binding.loader.bar.visibility = View.VISIBLE
     }
 
     override fun hideLoader() {
-       // binding.loader.bar.visibility = View.GONE
+        binding.loader.bar.visibility = View.GONE
     }
 }
--- app/src/main/java/com/latifapp/latif/ui/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/latifapp/latif/ui/auth/login/LoginViewModel.kt
@@ -1,8 +1,39 @@
 package com.latifapp.latif.ui.auth.login
 
+import android.util.Patterns
+import androidx.lifecycle.MutableLiveData
 import com.latifapp.latif.data.local.AppPrefsStorage
 import com.latifapp.latif.ui.base.BaseViewModel
 import javax.inject.Inject
 
 class LoginViewModel @Inject constructor(appPrefsStorage: AppPrefsStorage):BaseViewModel(appPrefsStorage) {
+
+    val validateLiveData= MutableLiveData<SignUpFiled>(null)
+    public fun login(username:String,password:String){
+        if (validate(username,password)){}
+    }
+
+    private fun validate(username:String,password:String):Boolean{
+        var valid =true
+        if (Patterns.EMAIL_ADDRESS.matcher(username).matches()||Patterns.PHONE.matcher(username).matches()){
+
+        }else {
+            validateLiveData.value= SignUpFiled.email_
+
+            valid=false
+        }
+
+        if (password.length>=6){
+
+        }else {
+            validateLiveData.value= SignUpFiled.password_
+            valid=false
+        }
+        validateLiveData.value= null
+        return valid
+    }
+
+    enum class SignUpFiled{
+        email_,password_,phone_,name_,address_,country_,city_,governorate_
+    }
 }
